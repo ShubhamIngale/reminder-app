664cbd8533abf81510226bf1029acc1f415fdbe5
ShubhamIngale <singale864@gmail.com>
2023-12-21 12:30:52 +0530

    fe: apiurl bug fixed

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = "http://localhost:3001/"
-// export const apiUrl = "https://reminder-app-2qz6.onrender.com/"
+// export const apiUrl = "http://localhost:3001/"
+export const apiUrl = "https://reminder-app-2qz6.onrender.com/"
